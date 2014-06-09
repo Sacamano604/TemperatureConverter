c0d4d193f0ed550ee38f4ee16f7a9c68a6a69429
Ben MacBook <info@van-markruudnew.jordancap.com>
2014-06-09 09:43:42 -0700

    Completed celsius to fahrenheit conversion

--- js/convertor.js
+++ js/convertor.js
@@ -1,8 +1,17 @@
-var makeFahrenheit = function(celcius) {
-	if (isNaN(celcius)) {
-		document.getElementById("messages").innerHTML = "<p id='errorMessage'>Sorry, please enter a valid number.</p>";		
+var makeFahrenheit = function(celsius) {
+	if (isNaN(celsius)) {
+		document.getElementById("celMessages").innerHTML = "<p id='errorMessage'>Sorry, please enter a valid number.</p>";		
 	} else {
-		var fahrenheit = ((celcius * 9)/5) + 32;
-		document.getElementById("messages").innerHTML = "<p id='result'>" + fahrenheit + " degrees fahrenheit";
+		var fahrenheit = ((celsius * 9)/5) + 32;
+		document.getElementById("celMessages").innerHTML = "<p id='result'>" + fahrenheit + " degrees fahrenheit</p>";
+	}
+};
+
+var makeCelsius = function(fahrenheit) {
+	if (isNaN(fahrenheit)) {
+		document.getElementById("farMessages").innerHTML = "<p id='errorMessage'>Sorry, please enter a valid number.</p>";
+	} else {
+		var celsius = ((fahrenheit - 32)*5) / 9;
+		document.getElementById("farMessages").innerHTML = "<p id='result'>" + celsius + " degrees celsius</p>";
 	}
 };
