9e5dcaad8120a33de240df0c4c6d234dfdf4caf5
Ben Toussi <btoussi@central1.com>
2017-01-10 09:46:29 -0800

    Cleaner and more efficient code

--- js/convertor.js
+++ js/convertor.js
@@ -1,17 +1,11 @@
-var makeFahrenheit = function(celsius) {
-	if (isNaN(celsius)) {
-		document.getElementById("celMessages").innerHTML = "<p id='errorMessage'>Sorry, please enter a valid number.</p>";		
-	} else {
-		var fahrenheit = ((celsius * 9)/5) + 32;
-		document.getElementById("celMessages").innerHTML = "<p id='result'>" + fahrenheit + " degrees fahrenheit</p>";
-	}
+var makeFahrenheit = function() {
+	var celsius = document.getElementById('celsius').value;
+	var fahrenheit = ((celsius * 9)/5) + 32;
+	document.getElementById('fahrenheit').value = fahrenheit;
 };
 
 var makeCelsius = function(fahrenheit) {
-	if (isNaN(fahrenheit)) {
-		document.getElementById("farMessages").innerHTML = "<p id='errorMessage'>Sorry, please enter a valid number.</p>";
-	} else {
-		var celsius = ((fahrenheit - 32)*5) / 9;
-		document.getElementById("farMessages").innerHTML = "<p id='result'>" + celsius + " degrees celsius</p>";
-	}
+	var fahrenheit = document.getElementById('fahrenheit').value;
+	var celsius = ((fahrenheit - 32)*5) / 9;
+	document.getElementById('celsius').value = celsius;
 };
